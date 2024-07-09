013545de68204f5c07b1fc8209760061f50084d7
Levisdroidcode <kimathilevis24@gmail.com>
2024-07-09 12:30:44 +0300

    Updated movie references in App.js and MovieCard.jsx

--- src/App.js
+++ src/App.js
@@ -1,74 +1,57 @@
-import {useEffect, useState} from 'react';
 
-import MovieCard from './MovieCard.jsx';
+import React, { useState, useEffect } from "react";
 
-import './App.css';
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
 
-import SearchIcon from './search.svg';
-
-const API_URL= 'http://ww.omdbapi.com?apikey=1d9d3f54'
-
-const movie1 = {
-    "Title": "Amazing Spiderman Syndrome",
-    "Year": "2012",
-    "imdbID": "tt2586634",
-    "Type": "movie",
-    "Poster": "N/A"
-}
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    
-    useEffect(() => {
-        searchMovies(`Spiderman`)
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
 
-    }, []);
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
 
-    const searchMovies = async (title) => {
+  const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
-    }
+  };
 
-    useEffect(() => {
-        searchMovies(`Spiderman`)
+  return (
+    <div className="app">
+      <h1>MuvieHub</h1>
 
-    }, []);
-    
-    return (
-        <div className="app">
-            <h1>MuvieHub</h1>
-            <div className="search">
-                <input
-                placeholder="Search for movies"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <img
-                src={SearchIcon}
-                alt="search"
-                onClick={() => searchMovies(searchTerm)}/>
-            </div>
-            {
-                movies?.length > 0
-                 ? (
-                    <div className='container'>
-                        {movies.map((movie) => (
-                            <MovieCard movie={movie} />
-                        ))}
-                    </div>
-                 ) : (
-                    <div classname="empty">
-                        <h2>No movies found</h2>
-                </div>
-                 )
-            }
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
 
-            
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
-    );
-}
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default App; 
+export default App;
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 
 const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
     return (
-        <div className="movie">
+        <div className="movie" key={imdbID}>
                 <div>
                     <p>{Year}</p>
                 </div>
+
                 <div>
                     <img src={Poster !== 'N/A' ? Poster : 'https://via.placeholder.com/400'} alt={Title}></img>
                 </div>
 
-            <div>
+                <div>
                 <span>{Type}</span>
                 <h3>{Title}</h3>
-            </div>
+                </div>
         </div>
     );
 }
